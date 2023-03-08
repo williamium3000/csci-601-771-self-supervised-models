28f3f9db702487a438527b3d8ef5ecf159260e9d
danielk <danyal.khashabi@gmai.com>
2023-03-08 12:08:57 -0500

    update

--- sp2023/hw6/launchpad.sh
+++ sp2023/hw6/launchpad.sh
@@ -2,11 +2,14 @@
 
 #SBATCH -A cs601_gpu
 #SBATCH --partition=mig_class
+#SBATCH --reservation=MIG
+#SBATCH --qos=qos_mig_class
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=12:00:00
 #SBATCH --job-name="HW6 CS 601.471/671 homework"
+
 
 module load anaconda
 
@@ -18,4 +21,4 @@ conda activate toy_classification_env # open the Python environment
 pip install -r requirements.txt # install Python dependencies
 
 # runs your code
-srun python classification.py  --experiment "overfit" --small_subset True --device cuda --model "distilbert-base-uncased" --batch_size "256" --lr 1e-4 --num_epochs 20
+srun python classification.py  --experiment "overfit" --small_subset True --device cuda --model "distilbert-base-uncased" --batch_size "32" --lr 1e-4 --num_epochs 20
